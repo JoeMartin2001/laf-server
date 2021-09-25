7e99f6c8e6caff407fde1001ebf591aac4386baf
serdarchik2001 <sardorbekaminjonov2001@gmail.com>
2021-09-25 17:22:22 +0500

    Added multer middleware and disabled x-powered-by

--- src/common/middlewares.ts
+++ src/common/middlewares.ts
@@ -3,10 +3,20 @@ import morgan from 'morgan';
 import authRouter from '../routes/auth.route';
 import itemRouter from '../routes/item.route';
 import cors from 'cors';
+import multer from 'multer';
+
+const multerMid = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
 const useMiddlewares = (app: Application) => {
+  app.disable('x-powered-by');
+  app.use(multerMid.single('file'));
   app.use(json());
-  app.use(urlencoded({ extended: true }));
+  app.use(urlencoded({ extended: false }));
   app.use(cors());
   app.use(morgan('dev'));
 
